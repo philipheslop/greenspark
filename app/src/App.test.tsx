2782d800c49c3d7f9e8fc14cbd0ff91cce2a23ca
Phil Heslop <philip.heslop@ncl.ac.uk>
2024-12-12 12:10:17 +0000

    Main App structure includes widgets

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+//import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+/*test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});*/
+
+//As a user I want to see my product widgets.
+test('3 wigdets are displayed', async() => {
+  render(<App />);
+  await waitFor(() => {
+    const widgetContainer = screen.getByText(/Per product widgets/i);
+    expect(widgetContainer).toBeInTheDocument();
+  })
+  await waitFor(()=>{
+    const widgets = screen.getAllByText(/This product/i);
+    expect(widgets).toHaveLength(3);
+  })
 });
+
+//As a user I want to customise my widgets by changing their colors, active state and whether it’s linked to my public profile (using client-side state management)
+
+//Only one widget can have the active state at a time
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,43 +1,47 @@
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
 import logo from './logo.svg';
 import './App.css';
+import { getWidgets, Widget } from './widgetUtils';
+import { WidgetsContainer } from './WIdgetsContainer';
 
-
+const queryClient = new QueryClient()
 
 function App() {
-  const [data,setData] = useState(null);
-  useEffect(()=>{
-
-    fetch("https://b795b019-1f84-41f4-93a3-a702d686c75a.mock.pstmn.io/product-widgets",{
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data[0].type);
-        console.log(data);
-      })
-      .catch(error=>console.error(error))
-    },[]);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload bananas.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          {data && <p>{data}</p>}
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <AppContents />
+    </QueryClientProvider>
   );
+}
+
+function AppContents() {
+      const { isLoading, error, data } = useQuery<Widget[]>({
+      queryKey: ['widgets'],
+      queryFn: getWidgets,
+    })
+
+    //const [activeWidget, setActiveWidget] = useState(null);
+  
+    if (isLoading) return 'Loading...'
+    if (error) return 'An error has occurred: ' + error.message
+  return (<div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload bananas.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+    <WidgetsContainer processedData={data?data:[]}></WidgetsContainer>
+  </div>)
 }
 
 export default App;
