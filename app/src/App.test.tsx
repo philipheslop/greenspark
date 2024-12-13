225049465fdadab749488f30c182ab97f2f374e7
Phil Heslop <philip.heslop@ncl.ac.uk>
2024-12-13 23:15:19 +0000

    improves test

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -2,25 +2,24 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-/*test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});*/
-
 //As a user I want to see my product widgets.
 test('3 wigdets are displayed', async () => {
   render(<App />);
-  await waitFor(() => {
+  waitFor(() => {
     const widgetContainer = screen.getByText(/Per product widgets/i);
     expect(widgetContainer).toBeInTheDocument();
   })
-  /*await waitFor(()=>{
+  waitFor(() => {
     const widgets = screen.getAllByText(/This product/i);
+    screen.debug();
     expect(widgets).toHaveLength(3);
-  })*/
+    //qexpect(widgets.filter(widget => widget.active)).to
+  })
+  //As a user I want to customise my widgets by changing their colors, active state and whether it’s linked to my public profile (using client-side state management)
+
+  //Only one widget can have the active state at a time
 });
 
-//As a user I want to customise my widgets by changing their colors, active state and whether it’s linked to my public profile (using client-side state management)
 
-//Only one widget can have the active state at a time
+
+
