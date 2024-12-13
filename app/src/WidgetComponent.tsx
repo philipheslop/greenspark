2e531c5a719436500c6fce5218b2a3af7dce6938
Phil Heslop <philip.heslop@ncl.ac.uk>
2024-12-13 23:14:41 +0000

    add tooltip

--- app/src/WidgetComponent.tsx
+++ app/src/WidgetComponent.tsx
@@ -57,7 +57,6 @@ const updateWidgetActive = (id: number, active: boolean) => {
 }
 
 export const WidgetsComponent: React.FC<WidgetComponentProps> = ({ widget }) => {
-
     return (
         <div className='widget-card'>
             <div style={{ '--widget-colour': widget.selectedColor, '--widget-text-colour': adjustBackgroundAndForegroundColour(widget.selectedColor) } as React.CSSProperties} className='widget-header'>
@@ -74,22 +73,28 @@ export const WidgetsComponent: React.FC<WidgetComponentProps> = ({ widget }) => 
                 </div>
             </div>
             <div className="widget-body">
-                <div className="widget-body-item-l">Linked:</div>
+                <div className="widget-body-item-l">
+                    Link to Public Profile
+                    <span className="widget-link-info">&#9432;
+                        <span className="tooltiptext">
+                            This widget links directly to your public profile so that you can easily share your impact with your customers. Turn it off here if you do not want the badge to link to it.
+                            <br></br>
+                            <br></br>
+                            <a href="http://www.google.com">View Public Profile</a>
+                        </span>
+                    </span>
+                </div>
                 <div className="widget-body-item-r">
                     <input type="checkbox" className="check_box" id={"checkbox" + widget.id} checked={widget.linked} onChange={() => updateWidgetLinked(widget.id, !widget.linked)} />
                     <label className="check_box_label" htmlFor={"checkbox" + widget.id} />
                 </div>
-                <div className="widget-body-item-l">Active:</div>
-                <div className="widget-body-item-r">
-                    <input type="checkbox" className="toggle" id={"toggle" + widget.id} checked={widget.active} onChange={() => updateWidgetActive(widget.id, !widget.active)} />
-                    <label className="toggle_label" htmlFor={"toggle" + widget.id} />
-                </div>
-                <div className="widget-body-item-l">Badge Colour: </div>
+                <div className="widget-body-item-l">Badge Colour</div>
                 <div className="widget-body-item-r">
                     <div className='widget-palette'>
                         {WidgetColourValues.map((colour: WidgetColor) => {
                             return (
                                 <div
+                                    key={colour + "_" + widget.id}
                                     style={
                                         {
                                             '--palette-colour': colour,
@@ -101,6 +106,11 @@ export const WidgetsComponent: React.FC<WidgetComponentProps> = ({ widget }) => 
                             );
                         })}
                     </div>
+                </div>
+                <div className="widget-body-item-l">Active badge</div>
+                <div className="widget-body-item-r">
+                    <input type="checkbox" className="toggle" id={"toggle" + widget.id} checked={widget.active} onChange={() => updateWidgetActive(widget.id, !widget.active)} />
+                    <label className="toggle_label" htmlFor={"toggle" + widget.id} />
                 </div>
             </div>
         </div>
